d2d985564a37623d86646229d29d006603fc55c4
MuhammadWaseem961 <60288560+MuhammadWaseem961@users.noreply.github.com>
2020-10-16 13:05:20 +0500

    1_05

--- l6flickerfone/resources/views/product_detail.blade.php
+++ l6flickerfone/resources/views/product_detail.blade.php
@@ -1479,45 +1479,45 @@ Up to 20 h (multimedia)
          </div>
 
           <!-- Modal -->
-                          <div id="myModal" class="modal fade" tabindex="" role="dialog" aria-labelledby="myModal" aria-hidden="true" style="margin-top: 5%;">
-                            <div class="modal-dialog">
+          <div id="myModal" class="modal fade" tabindex="" role="dialog" aria-labelledby="myModal" aria-hidden="true" style="margin-top: 5%;">
+            <div class="modal-dialog">
 
-                              <!-- Modal content-->
-                              <div class="modal-content">
-                                <div class="modal-header">
-                                  <h4 class="modal-title" style="color: black;">Sign in</h4>
-                                    
-                                </div>
-                                  <div class="modal-body">
-                                    <div class="row"><p class="alert alert-warning ErrorMsg"></p></div>
-                                    <form method="post">
-                                      @csrf
-                                      <div class="form-group">
-                                          <label class="info-title">Email</label>
-                                          <input type="text" name="login_email" id="username" class="form-control unicase-form-control text-input" value="{{ old('login_email') }}" required placeholder="Email">
-                                      </div>
-                                      <div class="form-group">
-                                          <label class="info-title">Password</label>
-                                          <input type="password" id="password" name="login_password" class="form-control unicase-form-control text-input" placeholder="Password" value="">
-                                      </div>  
+              <!-- Modal content-->
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h4 class="modal-title" style="color: black;">Sign in</h4>
+                    
+                </div>
+                  <div class="modal-body">
+                    <div class="row"><p class="alert alert-warning ErrorMsg"></p></div>
+                    <form method="post">
+                      @csrf
+                      <div class="form-group">
+                          <label class="info-title">Email</label>
+                          <input type="text" name="login_email" id="username" class="form-control unicase-form-control text-input" value="{{ old('login_email') }}" required placeholder="Email">
+                      </div>
+                      <div class="form-group">
+                          <label class="info-title">Password</label>
+                          <input type="password" id="password" name="login_password" class="form-control unicase-form-control text-input" placeholder="Password" value="">
+                      </div>  
 
-                                      <button type="button" class="btn-upper btn subbtn checkout-page-button modal_login">Login</button>
-                                      <a href="/password/reset" class=" col-md-offset-2 btn-upper btn subbtn checkout-page-button" style="background: #e30070;">Forget Password</a>
+                      <button type="button" class="btn-upper btn subbtn checkout-page-button modal_login">Login</button>
+                      <a href="/password/reset" class=" col-md-offset-2 btn-upper btn subbtn checkout-page-button" style="background: #e30070;">Forget Password</a>
 
 
-                                    </form>
-                                   
-                               
-                                
-                                  </div>
-                                <div class="modal-footer">
-                                  <button type="button" class="btn btn-default modal_close" data-dismiss="modal">Close</button>
-                                </div>
-                              </div>
+                    </form>
+                   
+               
+                
+                  </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-default modal_close" data-dismiss="modal">Close</button>
+                </div>
+              </div>
 
-                            </div>
-                          </div>
-                          <!-- End Modal --> 
+            </div>
+          </div>
+          <!-- End Modal --> 
 
 <div id="view2MobileZero" style="width:20%; float:left " ></div>
 </div>
